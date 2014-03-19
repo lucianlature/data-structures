27b2b97906060244a4b8ad4157393f79dea87e5b
lucianlature <lucian.lature@gmail.com>
2014-03-19 22:58:36 +0000

    More tests added to Stack.

--- src/Stack.js
+++ src/Stack.js
@@ -1,11 +1,38 @@
 'use strict';
 
+/**
+ * @constructor
+ * @description A stack is structured as an ordered collection of items
+ * where items are added to and removed from the end called the "top".
+ * Stacks are ordered LIFO.
+ */
 var Stack = function () {
 	this._items = [];
 };
 
+/**
+ * @description Adds a new item to the top of the stack
+ * @this {Stack}
+ * @param  {*} item Any type of items can be added
+ */
+Stack.prototype.push = function (item) {
+	this._items.push(item);
+};
+
+/**
+ * @description Checks if the stack is empty or not
+ * @return {Boolean} True if the stack is empty, false otherwise
+ */
 Stack.prototype.isEmpty = function () {
 	return this._items.length === 0;
+};
+
+/**
+ * @description  Returns the number of items in the stack
+ * @return {Number} Number of items
+ */
+Stack.prototype.size = function () {
+	return this._items.length;
 };
 
 module.exports = Stack;
--- test/Stack.spec.js
+++ test/Stack.spec.js
@@ -3,11 +3,38 @@
 var Stack = require('../src/Stack'),
 	expect = chai.expect;
 
-describe('Stack test', function() {
+describe('Stack test', function () {
 
-	it('should be able to instantiate a new Stack', function() {
-		var instance = new Stack();
-		expect(instance).to.be.an.instanceof(Stack);
+	var instance = null;
+
+	beforeEach(function (done) {
+		instance = new Stack();
+		done();
+	});
+
+	afterEach(function (done) {
+		instance = null;
+		done();
+	});
+
+	describe('Stack instantiation', function () {
+		it('should be able to instantiate a new Stack', function () {
+			expect(instance).to.be.an.instanceof(Stack);
+		});
+	});
+
+	describe('Stack isEmpty', function () {
+		it('should be true if the stack is empty', function () {
+			var isEmpty = instance.isEmpty();
+			expect(isEmpty).to.be.true;
+		});
+
+		it('should be false if the stack is not empty', function () {
+			instance.push('foo');
+			var isEmpty = instance.isEmpty();
+			expect(isEmpty).to.be.false;
+		});
+
 	});
 
 });
