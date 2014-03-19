0ec6396cf5afb9ed0324ba74e17178c7daac5b36
Lucian Lature <lucian.lature@gmail.com>
2014-03-19 20:30:58 +0000

    Few small changes.

--- test/Stack.spec.js
+++ test/Stack.spec.js
@@ -3,7 +3,7 @@ var Stack = require('../src/Stack'),
 
 describe('Stack test', function() {
 
-  it('should be able to use my Stack module', function() {
+it('should be able to use my Stack module', function() {
     if (Stack.echo() !==  'test') {
       throw new Error('echo method should return test');
     }
