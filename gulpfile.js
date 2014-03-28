b8a49c82c5c3ef599e532f524c49aedfe066a6dd
lucianlature <lucian.lature@gmail.com>
2014-03-28 23:10:21 +0000

    Using Gulp instead of Grunt for testing and stuff.

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,29 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
+// var mochaPhantomJS = require('gulp-mocha-phantomjs');
+var shell = require('gulp-shell');
 
 gulp.task('scripts', function() {
-	return gulp.src('./test/*.spec.js', {read: false})
+	gulp.src('./test/*.spec.js', {read: false})
 		.pipe(browserify({
 			debug: true
 		}))
+		.pipe(uglify())
 		.pipe(concat('test.gulp.js'))
-		.pipe(gulp.dest('./test'));
+		.pipe(gulp.dest('./test'))
+		.pipe(shell([
+			'mocha-phantomjs -R spec test/testrunner.html'
+		]));
 });
+/*
+gulp.task('test', function () {
+  return gulp
+  .src('./test/testrunner.html')
+  .pipe(mochaPhantomJS());
+});
+*/
 
-gulp.task('build', ['scripts']);
+gulp.task('default', ['scripts']);
