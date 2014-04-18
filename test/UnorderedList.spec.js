af6e7ac30eb29924b361420544366f415dd80a74
lucianlature <lucian.lature@gmail.com>
2014-04-18 10:56:19 +0100

    WIP - UnorderedList append tests.

--- test/UnorderedList.spec.js
+++ test/UnorderedList.spec.js
@@ -85,7 +85,7 @@ describe('UnorederedList test', function () {
 	describe('UnorderedList append', function () {
 		it('should add new items to the list', function () {
 			[3, 5, 8, 13, 21].forEach(function (item, index) {
-				instance.append(item);
+				// instance.append(item);
 				// expect(instance.size()).to.equal(index + 1);
 			});
 		});
