4c1399c7d59fa55da8feb17f6c69556f49f612a2
lucianlature <lucian.lature@gmail.com>
2014-04-23 20:58:36 +0100

    Missing 'us strict' statement.

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,3 @@
 */
 
 require('./gulp');
-
-/*
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var browserify = require('gulp-browserify');
-var mochaPhantomJS = require('gulp-mocha-phantomjs');
-
-gulp.task('browserify', function() {
-	gulp.src('./test/*.spec.js', {read: false})
-		.pipe(browserify({
-			debug: true
-		}))
-		.pipe(uglify())
-		.pipe(concat('test.gulp.js'))
-		.pipe(gulp.dest('./test'));
-});
-
-gulp.task('test', function () {
-	return gulp.src('test/testrunner.html').pipe(mochaPhantomJS());
-});
-
-gulp.task('default', ['browserify', 'test']);
-*/
