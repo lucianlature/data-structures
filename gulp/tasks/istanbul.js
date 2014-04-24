d042a64be273dcc90894639483183dcef26ceff3
lucianlature <lucian.lature@gmail.com>
2014-04-24 22:03:55 +0100

    Added gulp istanbul task.

---
+++ gulp/tasks/istanbul.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha'); // Using mocha here, but any test framework will work
+
+
+gulp.task('istanbul', function (cb) {
+  gulp.src(['src/*.js'])
+    .pipe(istanbul()) // Covering files
+    .on('end', function () {
+      gulp.src(['test/*.spec.js'])
+        .pipe(mocha())
+        .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+        .on('end', cb);
+    });
+});
