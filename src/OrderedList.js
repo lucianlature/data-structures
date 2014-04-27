fb6f0163cd17c28c613952ce5dfd85ad7d853bfa
Lucian Lature <lucian.lature@gmail.com>
2014-04-27 15:32:51 +0100

    Added ordered linked list structure.

---
+++ src/OrderedList.js
@@ -0,0 +1,190 @@
+'use strict';
+
+var Node = require('./Node');
+
+/**
+ * @constructor
+ * @description A list is a collection of items where each item holds a
+ * relative position that is based upon some underlying characterisitc of the item.
+ * The ordering is typically either ascending or descending - meaningful comparison operation
+ * must be defined.
+ */
+function OrderedList () {
+	this._head = null;
+	this._tail = null;
+}
+
+/**
+ * @description Checks if the list is empty or not
+ * @return {Boolean} True if the list is empty, false otherwise
+ */
+OrderedList.prototype.isEmpty = function () {
+	return this._head === null;
+};
+
+/**
+ * @description Adds a new item to the list
+ * @this {UnorderedList}
+ * @param  {Any} item Any type of items can be added
+ */
+UnorderedList.prototype.add = function (item) {
+    var temp = new Node (item);
+    temp.setNext(this._head);
+    this._head = temp;
+    this._tail = temp.getNext();
+};
+
+/**
+ * @description  Returns the number of items in the list
+ * @return {Number} Number of items
+ */
+UnorderedList.prototype.size = function () {
+	var current = this._head,
+		size = 0;
+
+	while (current !== null) {
+		size += 1;
+		current = current.getNext();
+	}
+
+	return size;
+};
+
+/**
+ * @description Searches for the item in the list
+ * @param  {Any} item
+ * @return {boolean} Returns true if item is found
+ */
+UnorderedList.prototype.search = function (item) {
+	var current = this._head,
+		found = false;
+
+	while (current !== null && !found) {
+		if (current.getData() === item) {
+			found = true;
+		} else {
+			current = current.getNext();
+		}
+	}
+
+	return found;
+};
+
+/**
+ * @description Removes the item from the list. It needs the item and modifies the list.
+ * @param  {Any} item
+ */
+UnorderedList.prototype.remove = function (item) {
+	var current = this._head,
+		previous = null,
+		found = false;
+
+	while (!found) {
+		if (current.getData() === item) {
+			found = true;
+		} else {
+			previous = current;
+			current = current.getNext();
+		}
+	}
+
+	if (previous === null) {
+		this._head = current.getNext();
+	} else {
+		previous.setNext(current.getNext());
+	}
+
+};
+
+/**
+ * @description Adds a new item to the end of the list making it the last item in the collection.
+ * @param  {Any} item
+ */
+UnorderedList.prototype.append = function (item) {
+	var temp = new Node (item);
+	if (this._tail === null) {
+		this._head = temp;
+		this._tail = temp;
+	} else {
+		this._tail.setNext(temp);
+		this._tail = temp;
+	}
+};
+
+/**
+ * @description Returns the position of the item in the list
+ * @param  {Any} item
+ * @return {Number} Index or -1 if this list does not contain the element.
+ */
+UnorderedList.prototype.indexOf = function (item) {
+	var current = this._head,
+		index = 0;
+
+	while (current !== null) {
+		if (current.getData() === item) {
+			return index;
+		} else {
+			current = current.getNext();
+		}
+		index += 1;
+	}
+
+	return -1;
+};
+
+/**
+ * @description Adds a new item to the list at a certain specified position
+ * @param  {Number} Position index
+ * @param  {Any} item
+ */
+UnorderedList.prototype.insert = function (position, item) {
+	var current = this._head,
+		temp = new Node (item),
+		index = 0;
+
+	while (current !== null) {
+
+		if (index === position - 1) {
+			var next = current.getNext();
+			current.setNext(temp);
+			temp.setNext(next);
+		}
+
+		current = current.getNext();
+		index += 1;
+	}
+};
+
+/**
+ * @description Removes and return the last item in the list.
+ * @param {Number} position If present, removes and returns the item at position pos.
+ * @return {*}
+ */
+UnorderedList.prototype.pop = function (position) {
+	var current = this._head,
+		previous = null,
+		index = 0;
+
+	while (current !== null) {
+
+		if (position && index === position) {
+			previous.setNext(current.getNext());
+			return current;
+		}
+
+		if (current.getNext() === null) {
+			if (previous !== null) {
+				this._tail = previous;
+				previous.setNext(null);
+			} else {
+				this._head = null;
+			}
+			return current;
+		}
+		previous = current;
+		current = current.getNext();
+		index += 1;
+	}
+};
+
+module.exports = OrderedList;
---
+++ test/OrderedList.spec.js
@@ -0,0 +1,148 @@
+'use strict';
+
+var List = require('../src/OrderedList'),
+	chai = require('chai'),
+	expect = chai.expect,
+	instance = null;
+
+describe('OrderedList test', function () {
+
+	beforeEach(function (done) {
+		instance = new List();
+		done();
+	});
+
+	afterEach(function (done) {
+		instance = null;
+		done();
+	});
+
+	describe('UnorderedList instantiation', function () {
+		it('should be able to instantiate a new unordered list', function () {
+			expect(instance).to.be.an.instanceof(List);
+		});
+	});
+	/*
+	describe('UnorderedList isEmpty', function () {
+		it('should return true when it does not have a head', function () {
+			expect(instance.isEmpty()).to.be.true;
+		});
+	});
+
+	describe('UnorderedList size', function () {
+		it('should return 0 when first instantiated', function () {
+			expect(instance.size()).to.equal(0);
+		});
+		it('should return the correct size when items are being added', function () {
+			instance.add('foo');
+			expect(instance.size()).to.equal(1);
+			instance.add('bar');
+			expect(instance.size()).to.equal(2);
+		});
+	});
+
+	describe('UnorderedList add', function () {
+		it('should add new items to the list', function () {
+			[3, 5, 8, 13, 21].forEach(function (item, index) {
+				instance.add(item);
+				expect(instance.size()).to.equal(index + 1);
+			});
+		});
+	});
+
+	describe('UnorderedList search', function () {
+		it('should add new items to the list', function () {
+			[3, 5, 8, 13, 21].forEach(function (item) {
+				instance.add(item);
+			});
+			expect(instance.search(3)).to.be.true;
+			expect(instance.search(5)).to.be.true;
+			expect(instance.search(8)).to.be.true;
+			expect(instance.search(13)).to.be.true;
+			expect(instance.search(21)).to.be.true;
+			expect(instance.search(0)).to.be.false;
+		});
+	});
+
+	describe('UnorderedList remove', function () {
+		it('should add new items to the list', function () {
+			[3, 5, 8, 13, 21].forEach(function (item) {
+				instance.add(item);
+			});
+			instance.remove(3);
+			expect(instance.size()).to.equal(4);
+			expect(instance.search(3)).to.be.false;
+			instance.remove(21);
+			expect(instance.size()).to.equal(3);
+			expect(instance.search(21)).to.be.false;
+			instance.remove(8);
+			expect(instance.size()).to.equal(2);
+			expect(instance.search(8)).to.be.false;
+			expect(instance.search(5)).to.be.true;
+			expect(instance.search(13)).to.be.true;
+		});
+	});
+
+	describe('UnorderedList append', function () {
+		it('should add new items to the list', function () {
+			[3, 5, 8, 13, 21].forEach(function (item, index) {
+				instance.append(item);
+				expect(instance.size()).to.equal(index + 1);
+			});
+		});
+	});
+
+	describe('UnorderedList indexOf', function () {
+		it('should return the correct position of the item in the list', function () {
+			[3, 5, 8, 13, 21].forEach(function (item) {
+				instance.add(item);
+			});
+			expect(instance.indexOf(3)).to.equal(4);
+			expect(instance.indexOf(5)).to.equal(3);
+			expect(instance.indexOf(8)).to.equal(2);
+			expect(instance.indexOf(13)).to.equal(1);
+			expect(instance.indexOf(21)).to.equal(0);
+			expect(instance.indexOf(1)).to.equal(-1);
+		});
+	});
+
+	describe('UnorderedList insert', function () {
+		it('should insert the item at the correct position in the list', function () {
+			[3, 5, 8, 13, 21].forEach(function (item) {
+				instance.add(item);
+			});
+			instance.insert(2, 7);
+			expect(instance.indexOf(7)).to.equal(2);
+			expect(instance.indexOf(8)).to.equal(3);
+		});
+	});
+
+	describe('UnorderedList pop', function () {
+		it('should remove the last item of the list if position not provided', function () {
+			var item;
+			instance.add(3);
+			instance.add(5);
+			instance.add(8);
+			item = instance.pop();
+			expect(item.getData()).to.equal(3);
+			item = instance.pop();
+			expect(item.getData()).to.equal(5);
+			item = instance.pop();
+			expect(item.getData()).to.equal(8);
+			expect(instance.isEmpty()).to.be.true;
+		});
+		it('should remove the item of the list at the provided position', function () {
+			var item;
+			instance.add(3);
+			instance.add(5);
+			instance.add(8);
+			item = instance.pop(1);
+			expect(item.getData()).to.equal(5);
+			item = instance.pop(0);
+			expect(item.getData()).to.equal(3);
+			instance.pop();
+			expect(instance.isEmpty()).to.be.true;
+		});
+	});
+	*/
+});
