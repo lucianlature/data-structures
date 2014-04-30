8e4bb5963c25b5d92ddda24a209ad5d388cc0847
Lucian Lature <lucian.lature@gmail.com>
2014-04-30 22:30:52 +0100

    Clean OrderedList code.

--- src/OrderedList.js
+++ src/OrderedList.js
@@ -2,26 +2,9 @@
 
 var Node = require('./Node'),
 	UnorderedList = require('./UnorderedList'),
+	extend = require('../lib/extend'),
 	sortingOperation = function (a, b) {
 		return a - b;
-	},
-	__slice = [].slice,
-	extend = function () {
-		var consumer = arguments[0],
-	      providers = __slice.call(arguments, 1),
-	      key,
-	      i,
-	      provider;
-
-	  for (i = 0; i < providers.length; ++i) {
-	    provider = providers[i];
-	    for (key in provider) {
-	      if (provider.hasOwnProperty(key)) {
-	        consumer[key] = provider[key];
-	      };
-	    };
-	  };
-	  return consumer;
 	};
 
 /**
