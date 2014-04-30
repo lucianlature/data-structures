1f5efbec8cfbeac456f541b38ce474141419ac54
Lucian Lature <lucian.lature@gmail.com>
2014-04-30 21:33:44 +0100

    Simple refactor by mixing the methods of UnorderedList into OrderedList.

--- src/OrderedList.js
+++ src/OrderedList.js
@@ -1,8 +1,27 @@
 'use strict';
 
 var Node = require('./Node'),
+	UnorderedList = require('./UnorderedList'),
 	sortingOperation = function (a, b) {
 		return a - b;
+	},
+	__slice = [].slice,
+	extend = function () {
+		var consumer = arguments[0],
+	      providers = __slice.call(arguments, 1),
+	      key,
+	      i,
+	      provider;
+
+	  for (i = 0; i < providers.length; ++i) {
+	    provider = providers[i];
+	    for (key in provider) {
+	      if (provider.hasOwnProperty(key)) {
+	        consumer[key] = provider[key];
+	      };
+	    };
+	  };
+	  return consumer;
 	};
 
 /**
@@ -18,159 +37,40 @@ function OrderedList () {
 	this._length = 0;
 }
 
-/**
- * @description Checks if the list is empty or not
- * @return {Boolean} True if the list is empty, false otherwise
- */
-OrderedList.prototype.isEmpty = function () {
-	return this._head === null;
-};
+extend(OrderedList.prototype, UnorderedList.prototype, {
 
-/**
- * @description Adds a new item to the list
- * @this {UnorderedList}
- * @param  {Any} item Any type of items can be added
- */
-OrderedList.prototype.add = function (item) {
-	var current = this._head,
-		previous = null,
-		stop = false,
-		temp = new Node(item);
+	/**
+	 * @description Adds a new item to the list
+	 * @this {UnorderedList}
+	 * @param  {Any} item Any type of items can be added
+	 */
+	add: function (item) {
+		var current = this._head,
+			previous = null,
+			stop = false,
+			temp = new Node(item);
 
-	while (current !== null && !stop) {
-		if (sortingOperation(current.getData(), item) > 0) {
-			stop = true;
-		} else {
-			previous = current;
-			current = current.getNext();
+		while (current !== null && !stop) {
+			if (sortingOperation(current.getData(), item) > 0) {
+				stop = true;
+			} else {
+				previous = current;
+				current = current.getNext();
+			}
 		}
-	}
 
-	if (previous === null) {
-		temp.setNext(this._head);
-		this._head = temp;
-	} else {
-		temp.setNext(current);
-		previous.setNext(temp);
-	}
-
-	this._length += 1;
-
-};
-
-/**
- * @description  Returns the number of items in the list
- * @return {Number} Number of items
- */
-OrderedList.prototype.size = function () {
-	return this._length;
-};
-
-/**
- * @description Searches for the item in the list
- * @param  {Any} item
- * @return {boolean} Returns true if item is found
- */
-OrderedList.prototype.search = function (item) {
-	var current = this._head,
-		found = false;
-
-	while (current !== null && !found) {
-		if (current.getData() === item) {
-			found = true;
+		if (previous === null) {
+			temp.setNext(this._head);
+			this._head = temp;
 		} else {
-			current = current.getNext();
+			temp.setNext(current);
+			previous.setNext(temp);
 		}
+
+		this._length += 1;
 	}
 
-	return found;
-};
+});
 
-/**
- * @description Removes the item from the list. It needs the item and modifies the list.
- * @param  {Any} item
- */
-OrderedList.prototype.remove = function (item) {
-	var current = this._head,
-		previous = null,
-		found = false;
-
-	// check if item is in the list
-	if (this.indexOf(item) === -1) {
-		return;
-	}
-
-	while (!found) {
-		if (current.getData() === item) {
-			found = true;
-		} else {
-			previous = current;
-			current = current.getNext();
-		}
-	}
-
-	if (previous === null) {
-		this._head = current.getNext();
-	} else {
-		previous.setNext(current.getNext());
-	}
-
-	this._length -= 1;
-
-};
-
-/**
- * @description Returns the position of the item in the list
- * @param  {Any} item
- * @return {Number} Index or -1 if this list does not contain the element.
- */
-OrderedList.prototype.indexOf = function (item) {
-	var current = this._head,
-		index = 0;
-
-	while (current !== null) {
-		if (current.getData() === item) {
-			return index;
-		} else {
-			current = current.getNext();
-		}
-		index += 1;
-	}
-
-	return -1;
-};
-
-/**
- * @description Removes and return the last item in the list.
- * @param {Number} position If present, removes and returns the item at position pos.
- * @return {*}
- */
-OrderedList.prototype.pop = function (position) {
-	var current = this._head,
-		previous = null,
-		foundPos = false,
-		position = (typeof position === 'undefined') ? this.size() - 1 : position,
-		index = 0;
-
-	while (!foundPos) {
-		if (index === position) {
-			foundPos = true;
-		} else {
-			previous = current;
-			current = current.getNext();
-			index += 1;
-		}
-	}
-
-	if (previous === null) {
-		this._head = current.getNext();
-	} else {
-		previous.setNext(current.getNext());
-	}
-
-	this._length -= 1;
-
-	return current;
-};
 
 module.exports = OrderedList;
