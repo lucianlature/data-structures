3f8575aafe705235c14fe6d96aa4c84793933786
Lucian Lature <lucian.lature@gmail.com>
2014-05-13 21:49:04 +0100

    Added BinaryTree as new data structure.

---
+++ src/BinaryTree.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @constructor
+ * @description A tree data structure in which each node has at most two children (referred to as the left child and
+ * the right child).
+ */
+function BinaryTree () {
+    this._root = null;
+}
+
+module.exports = BinaryTree;
