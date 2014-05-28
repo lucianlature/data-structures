739d419b57f538c32dac29a3bd0ee5570026ac8a
Lucian Lature <lucian.lature@gmail.com>
2014-05-28 20:58:40 +0100

    Another attempt to make bump work.

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    bump = require('gulp-bump');
 
 gulp.task('default', ['browserify', 'istanbul'], function () {
     gulp.src('./package.json')
