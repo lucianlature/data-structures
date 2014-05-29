2b955123f42e4703f4cbfba8ce02b4aba7d7b343
Lucian Lature <lucian.lature@gmail.com>
2014-05-29 20:54:42 +0100

    Push distribution automatically to master.

---
+++ util/dist.sh
@@ -0,0 +1,18 @@
+  cp -R dist $HOME/dist
+  cd $HOME
+
+  git config --global user.email "lucian.lature@gmail.com"
+  git config --global user.name "Lucian Lature"
+  git clone https://lucianlature:bdff5a7db8f45cb829d00af184316ca3b11ff78e@github.com/lucianlature/data-structures.git --branch=master master
+
+  cd master
+  git remote rm origin
+  git remote add origin https://lucianlature:bdff5a7db8f45cb829d00af184316ca3b11ff78e@github.com/lucianlature/data-structures.git
+
+  mkdir dist
+  cd dist
+  cp -Rf $HOME/dist/* .
+
+  git add -f .
+  git commit -m "Travis dist $TRAVIS_BUILD_NUMBER pushed to master"
+  git push -fq origin master
