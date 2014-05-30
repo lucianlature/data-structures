cd9d6fda68b83a5238b58fcfa7c60a6f63612e57
lucianlature <lucian.lature@gmail.com>
2014-05-30 20:53:29 +0100

    Clean-up. Removed all the old tokens.

--- util/dist.sh
+++ util/dist.sh
@@ -1,14 +1,8 @@
   cp -R dist $HOME/dist
   cd $HOME
 
-  git config --global user.email "lucian.lature@gmail.com"
-  git config --global user.name "lucianlature"
-  git clone https://lucianlature:bdff5a7db8f45cb829d00af184316ca3b11ff78e@github.com/lucianlature/data-structures.git --branch=master master
-
+  git clone https://github.com/lucianlature/data-structures.git --branch=master master
   cd master
-  git remote rm origin
-  git remote add origin https://lucianlature:bdff5a7db8f45cb829d00af184316ca3b11ff78e@github.com/lucianlature/data-structures.git
-
   mkdir dist
   cd dist
   cp -Rf $HOME/dist/* .
