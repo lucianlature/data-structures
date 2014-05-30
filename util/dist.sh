7c1b078cc09ed4bcbb09377c2d10f8beae402d36
lucianlature <lucian.lature@gmail.com>
2014-05-30 21:58:20 +0100

    Another attempt to setup the build process automatically.

--- util/dist.sh
+++ util/dist.sh
@@ -1,13 +1,15 @@
-  cp -R dist $HOME/dist
-  cd $HOME
+cp -R dist $HOME/dist
+cd $HOME
+git clone https://github.com/lucianlature/data-structures.git --branch=master master
+cd master
+git remote rm origin
+git remote add origin https://lucianlature:${GH_TOKEN}@github.com/lucianlature/data-structures.git
 
-  git clone https://github.com/lucianlature/data-structures.git --branch=master master
-  cd master
-  mkdir dist
-  cd dist
-  cp -Rf $HOME/dist/* .
+mkdir dist
+cd dist
+cp -Rf $HOME/dist/* .
 
-  git add -f .
-  git commit -m "Travis dist $TRAVIS_BUILD_NUMBER pushed to master"
-  git push -fq origin master
-  echo -e "Done with magic\n"
+git add -f .
+git commit -m "Travis dist $TRAVIS_BUILD_NUMBER pushed to master"
+git push -fq origin master
+echo -e "Done with magic\n"
