d1c69971df6d1e974bf43d8f6fb2f5fe586a2dd2
lucianlature <lucian.lature@gmail.com>
2014-06-01 19:47:45 +0100

    Do not modify package.json automatically as it triggers Travis to build forever.

--- util/dist.sh
+++ util/dist.sh
@@ -1,5 +1,5 @@
 cp -R dist $HOME/dist
-cp -R package.json $HOME/package.json
+#cp -R package.json $HOME/package.json
 
 cd $HOME
 git clone https://github.com/lucianlature/data-structures.git --branch=master master
@@ -11,8 +11,8 @@ rm -rf dist
 mkdir dist
 cd dist
 cp -Rf $HOME/dist/* .
-cd ..
-cp -Rf $HOME/package.json package.json
+#cd ..
+#cp -Rf $HOME/package.json package.json
 
 git add -f .
 git commit -m "Travis dist $TRAVIS_BUILD_NUMBER pushed to master"
