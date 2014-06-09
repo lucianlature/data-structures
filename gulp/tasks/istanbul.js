97b69eeea71daefcabf42c7498fc3f50ab137897
lucianlature <lucian.lature@gmail.com>
2014-06-09 23:04:55 +0100

    Modified callback name from 'name' to 'finish'.

--- gulp/tasks/istanbul.js
+++ gulp/tasks/istanbul.js
@@ -7,7 +7,7 @@ var gulp = require('gulp'),
 gulp.task('istanbul', function (cb) {
     gulp.src(['src/*.js'])
         .pipe(istanbul()) // Covering files
-        .on('end', function () {
+        .on('finish', function () {
             gulp.src(['test/*.spec.js'])
                 .pipe(mocha())
                 .pipe(istanbul.writeReports()) // Creating the reports after tests runned
