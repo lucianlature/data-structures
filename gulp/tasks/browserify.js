5f32a158d9968e2b6c166f1daaafdbdb27543d60
lucianlature <lucian.lature@gmail.com>
2014-06-10 20:12:44 +0100

    Code clean-up in tasks/browserify.js.

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,31 +1,31 @@
 'use strict';
 
-var browserify   = require('browserify');
-var gStreamify   = require('gulp-streamify');
-var uglify       = require('gulp-uglify');
-var concat       = require('gulp-concat');
-var gulp         = require('gulp');
-var handleErrors = require('../util/handleErrors');
-var source       = require('vinyl-source-stream');
-var glob         = require('glob');
+var browserify   = require('browserify'),
+    gStreamify   = require('gulp-streamify'),
+    uglify       = require('gulp-uglify'),
+    concat       = require('gulp-concat'),
+    gulp         = require('gulp'),
+    handleErrors = require('../util/handleErrors'),
+    source       = require('vinyl-source-stream'),
+    glob         = require('glob');
 
 gulp.task('browserify', function () {
 
-	var bundler = browserify();
-	glob.sync('./test/*.spec.js').forEach(function (file) {
-		bundler.add(file);
-	});
+    var bundler = browserify();
+    glob.sync('./test/*.spec.js').forEach(function (file) {
+        bundler.add(file);
+    });
 
-	bundle(bundler, 'data-structures.js')
-		.pipe(gStreamify(uglify()))
-		.pipe(gStreamify(concat('data-structures.min.js')))
-		.pipe(gulp.dest('./dist'));
+    bundle(bundler, 'data-structures.js')
+        .pipe(gStreamify(uglify()))
+        .pipe(gStreamify(concat('data-structures.min.js')))
+        .pipe(gulp.dest('./dist'));
 });
 
 
 function bundle (bundler, outFilename) {
-	return bundler
-		.bundle()
-		.on('error', handleErrors)
-		.pipe(source(outFilename));
+    return bundler
+        .bundle()
+        .on('error', handleErrors)
+        .pipe(source(outFilename));
 }
